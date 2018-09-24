7637e21f398964274e2522c0eeac34211c6b9fdb
ddsfecdcl <244744021@qq.com>
2018-09-24 18:00:54 +0800

    1.0.0.1测试更换主页模板

---
+++ mdx2/src/components/Home.vue
@@ -0,0 +1,23 @@
+// ddsfec
+// 首页
+<template >
+    <div id="home">
+      <ul >
+          <li v-for="item in tablist" :key="item">{{item}}</li>
+      </ul>
+      <h1>home</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      tabname: '菜单',
+      tablist: ['tab1', 'tab2', 'tab3'],
+      show: true
+    }
+  }
+}
+</script>
